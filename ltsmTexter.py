153227043aebcbeb7b4cac9998e0611ce2ae2a0f
miriku <me@miriku.com>
2017-08-24 10:40:11 -0700

    moved to python 3 and added result of week long weights generation

--- ltsmTexter.py
+++ ltsmTexter.py
@@ -7,9 +7,13 @@ from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
+import codecs
+
+sys.stdout = codecs.getwriter('latin-1')(sys.stdout.buffer, 'strict')
+
 # load ascii text and covert to lowercase
 filename = "names.txt"
-raw_text = open(filename).read()
+raw_text = open(filename, encoding="latin-1").read()
 raw_text = raw_text.lower()
 # create mapping of unique chars to integers, and a reverse mapping
 chars = sorted(list(set(raw_text)))
@@ -18,8 +22,8 @@ int_to_char = dict((i, c) for i, c in enumerate(chars))
 # summarize the loaded data
 n_chars = len(raw_text)
 n_vocab = len(chars)
-print "Total Characters: ", n_chars
-print "Total Vocab: ", n_vocab
+print("Total Characters: ", n_chars)
+print("Total Vocab: ", n_vocab)
 # prepare the dataset of input to output pairs encoded as integers
 seq_length = 100
 dataX = []
@@ -30,7 +34,7 @@ for i in range(0, n_chars - seq_length, 1):
 	dataX.append([char_to_int[char] for char in seq_in])
 	dataY.append(char_to_int[seq_out])
 n_patterns = len(dataX)
-print "Total Patterns: ", n_patterns
+print("Total Patterns: ", n_patterns)
 # reshape X to be [samples, time steps, features]
 X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
 # normalize
@@ -45,14 +49,14 @@ model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 # load the network weights
-filename = "weights-improvement-28-2.0712-bigger.hdf5"
+filename = "weights-categories.hdf5"
 model.load_weights(filename)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 # pick a random seed
 start = numpy.random.randint(0, len(dataX)-1)
 pattern = dataX[start]
-print "Seed:"
-print "\"", ''.join([int_to_char[value] for value in pattern]), "\""
+print ("Seed:")
+print ("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
 # generate characters
 for i in range(1000):
 	x = numpy.reshape(pattern, (1, len(pattern), 1))
@@ -64,4 +68,4 @@ for i in range(1000):
 	sys.stdout.write(result)
 	pattern.append(index)
 	pattern = pattern[1:len(pattern)]
-print "\nDone."
+print ("\nDone.")
